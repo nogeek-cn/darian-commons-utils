16a8c81da5918ce8f71efa25467ffdd118611448
Darian <1934849492@qq.com>
2020-12-14 20:15:41 +0800

    BeanNameGeneratorSearch

---
+++ darian-commons-spring/src/main/java/com/darian/spring/beanNameGenerator/BeanNameGeneratorSearch.java
@@ -0,0 +1,31 @@
+package com.darian.spring.beanNameGenerator;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.util.ClassUtils;
+
+import java.beans.Introspector;
+
+public class BeanNameGeneratorSearch {
+
+    public static void main(String[] args) {
+        String beanName = getBeanName(BBA.class);
+        System.out.println(beanName);
+        // beanNameGeneratorSearch.BBA
+
+        // 前两个字母大写直接返回的 ClassName
+    }
+
+    public static String getBeanName(Class<?> clazz) {
+        BeanDefinition definition = BeanDefinitionBuilder
+                .genericBeanDefinition(clazz)
+                .getBeanDefinition();
+        String shortClassName = ClassUtils.getShortName(definition.getBeanClassName());
+
+        return Introspector.decapitalize(shortClassName);
+    }
+
+    public static class BBA {
+
+    }
+}
