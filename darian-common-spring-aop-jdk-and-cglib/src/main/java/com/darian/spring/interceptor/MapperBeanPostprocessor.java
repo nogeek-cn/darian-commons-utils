6bfa64731600f2b0e59492f4b6cc804861e12c2d
darian <1934849492@qq.com>
2021-07-12 22:13:48 +0800

    2021-07-12 22:13:48 AutoCommit.sh by Darian

--- darian-common-spring-aop-jdk-and-cglib/src/main/java/com/darian/spring/interceptor/MapperBeanPostprocessor.java
+++ darian-common-spring-aop-jdk-and-cglib/src/main/java/com/darian/spring/interceptor/MapperBeanPostprocessor.java
@@ -2,6 +2,7 @@ package com.darian.spring.interceptor;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
 
 import java.lang.reflect.Proxy;
 
@@ -11,6 +12,7 @@ import java.lang.reflect.Proxy;
  * @author <a href="mailto:1934849492@qq.com">Darian</a>
  * @date 2021/7/9  下午12:40
  */
+@Configuration
 public class MapperBeanPostprocessor implements BeanPostProcessor {
 
     @Override
@@ -18,6 +20,11 @@ public class MapperBeanPostprocessor implements BeanPostProcessor {
 //        Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
 //                bean,
 //                new DALInvocationHandler());
+        if (beanName.equals("dalMapperImpl")) {
+            System.out.println(bean.getClass());
+        }
+
+//        System.out.println("MapperBeanPostprocessor"+"-:-"+beanName + "-:-");
         return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
     }
 
